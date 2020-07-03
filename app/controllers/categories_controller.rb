07c7be9392a9fa62359f4379dcbfad315088b2d3
umetake820 <umetac82@gmail.com>
2020-07-03 19:15:34 +0900

    本番環境用カテゴリー登録

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,25 @@
 class CategoriesController < ApplicationController
+
+  def new
+    if current_user.id == 1
+      @category = Category.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    Category.create(category_params)
+  end
+
   def show
     category = Category.find(params[:id])
     gon.quizzes = category.quizzes.sample(5)
     gon.images = Image.includes(:user).sample(5)
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
