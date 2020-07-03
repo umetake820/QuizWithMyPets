897f016b6e57383d0bb67d5d1aa27dbe286b8e95
umetake820 <umetac82@gmail.com>
2020-07-03 18:07:24 +0900

    categories/new.html.haml追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "quizzes#index"
   resources :quizzes
-  resources :categories, only: :show
+  resources :categories, only: [:new, :show]
   resources :images
   resources :users
 end
